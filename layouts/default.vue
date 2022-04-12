0f5b2f04bd364ca5ccc1e1aeed8ccb03af35747e
ChaoLiou <ucnsaythtagn@gmail.com>
2022-04-13 00:02:04 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="layout__default">
+    <header></header>
+    <div></div>
+    <main>
+      <slot />
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<style scoped>
+.layout__default {
+  background-color: #1a1a1a;
+  color: white;
+}
+</style>
